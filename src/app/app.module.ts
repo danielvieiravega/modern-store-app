2f3ec6a85999ffede20466069d5e48f732b6635a
Daniel Vieira Vega <danielv.vega@outlook.com>
2018-02-18 20:10:48 -0300

    criado o projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,46 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+// Rotas
+import { Routing, RoutingProviders } from './app.routing';
+
+// root
 import { AppComponent } from './app.component';
+
+// shared
+import { HeadbarComponent } from './components/shared/headbar/headbar.component';
+import { SubMenuComponent } from './components/shared/sub-menu/sub-menu.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+
+// components
+import { ProductListComponent } from './components/product-list/product-list.component';
+
+// pages
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { SignupPageComponent } from './pages/signup-page/signup-page.component';
+import { CartPageComponent } from './pages/cart-page/cart-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeadbarComponent,
+    SubMenuComponent,
+    ProductListComponent,
+    FooterComponent,
+    HomePageComponent,
+    LoginPageComponent,
+    SignupPageComponent,
+    CartPageComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    ReactiveFormsModule,
+    HttpModule,
+    Routing
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/pages/login-page/login-page.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { Validators, FormBuilder, FormGroup } from '@angular/forms';
+import { validateConfig } from '@angular/router/src/config';
+
+@Component({
+  selector: 'app-login-page',
+  templateUrl: './login-page.component.html'
+})
+export class LoginPageComponent implements OnInit {
+
+  public form: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      email: ['', Validators.compose([
+        Validators.minLength(5),
+        Validators.maxLength(160),
+        Validators.required
+      ])],
+      password: ['', Validators.compose([
+        Validators.minLength(6),
+        Validators.maxLength(160),
+        Validators.required
+      ])]
+    });
+  }
+
+  ngOnInit() { }
+
+}
